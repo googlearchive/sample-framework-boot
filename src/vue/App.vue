790f6441333e7713ba7b4c2017e5833425160b37
Evan You <yyx990803@gmail.com>
2017-01-16 10:37:26 -0500

    remove the use of progressive render

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -6,19 +6,16 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci mauris, euismod quis imperdiet eu, lacinia in leo. Mauris in orci sit amet eros finibus molestie. Vivamus laoreet, nibh eget ultrices consequat, leo mauris suscipit libero, eget feugiat enim turpis ac neque. Nam venenatis dolor ac posuere rhoncus.</p>
       <p>In eget finibus sapien, sit amet tempus augue. Pellentesque eu interdum nulla, sit amet dictum turpis. Sed ut lorem quis purus aliquet egestas. Nulla id risus ex. Morbi et lacinia risus. Integer eget ornare arcu. Donec purus est, dignissim a ante a, lacinia placerat massa. Donec quis lacinia lorem.</p>
     </div>
-    <section class="post__comments">
-      <h1>Comments</h1>
-      <comment v-for="comment in commentData" :comment="comment"/>
-    </section>
+    <comments :comments="commentData"></comments>
   </section>
 </template>
 
 <script>
 import commentData from '../shared/comments.js';
-import Comment from './Comment.vue';
+import Comments from './Comments.vue';
 
 export default {
-  components: { Comment },
+  components: { Comments },
   data: () => ({ commentData })
 }
 </script>
---
+++ src/vue/Comments.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="post__comments">
+    <h1>Comments</h1>
+    <div v-for="comment in comments" class="post__comment">
+      <h2 class="post__comment-author">{{ comment.username }} wrote</h2>
+      <p class="post__comment-text">
+        {{ comment.text }}
+      </p>
+      <vote-buttons
+        :score="comment.score"
+        @upvote="comment.score++"
+        @downvote="comment.score--">
+      </vote-buttons>
+    </div>
+  </section>
+</template>
+
+<script>
+import VoteButtons from './VoteButtons.vue'
+
+export default {
+  props: ['comments'],
+  components: { VoteButtons }
+}
+</script>
